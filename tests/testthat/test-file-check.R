7616f23b1355db2bb0e58eaabffd0e9324a2af72
matthew <matthew@harmonic.co.nz>
2020-03-26 15:00:09 +1300

    Update tests
    Former-commit-id: 6daaf09182ff1a220923ef32bb4fe06b80c4f4f0

--- tests/testthat/test-file-check.R
+++
@@ -1,5 +0,0 @@
-test_that("Source URL Exists", {
-  expect_true(RCurl::url.exists('https://www3.stats.govt.nz/2018census/sa1dataset2018/Statistical%20Area%201%20dataset%20for%20Census%202018%20%E2%80%93%20total%20New%20Zealand%20%E2%80%93%20CSV.zip'))
-})
-
-
