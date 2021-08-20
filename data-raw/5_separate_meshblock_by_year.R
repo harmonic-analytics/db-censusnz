16e2f753b173c4e8003d39f396b2af68861a1b13
Yuki Fujita <yuki@harmonic.co.nz>
2021-08-20 16:19:55 +1200

    correct variable names as in the dictionary and save by year

--- data-raw/5_separate_meshblock_by_year.R
+++ data-raw/5_separate_meshblock_by_year.R
@@ -15,21 +15,31 @@ library(janitor)
 
 # Importing Data ----------------------------------------------------------
 meshblock_dir = './data-raw/meshblock'
-database2 <- readRDS(file = paste0(meshblock_dir, "/meshblockdata_longformat_list.rds"))
-database2 %>%  names()
+database <- readRDS(file = paste0(meshblock_dir, "/meshblockdata_longformat_list.rds"))
+database %>%  names()
 save_data_dir <- "./data"
+# join data to get the same variable name as in dictionary
+df_mesh_vars <- read.csv('./data/meshblock_var.csv',
+                         header=TRUE,
+                         stringsAsFactors = FALSE)
+
 
 # functions ---------------------------------------------------------------
+
+# function to have the same variable as in dictionary
+fn_correct_variables <- function(data, subcategory){
+    newdata = data %>%
+        mutate(variable_name = gsub("\\.", "_", variable_name),
+               subcategory_m = subcategory)
+    names(newdata)[3:4] = c("variable", "variable_name")
+    fina_data = newdata %>%
+        left_join(df_mesh_vars, by = "variable") %>%
+        dplyr::select(meshblock, year, variable_dictionary,
+                      variable_name, count, subcategory_m)
+}
+
 # function to separate data by year
-split_by_year_fn <- function(data,
-                             category_name = "Individual",
-                             category_number){
-    data = data %>%
-        dplyr::mutate(variable_name = gsub("\\.", "_", variable_name)) %>%
-        rename("variable_group" = "variable",
-               "variable" = "variable_name") %>%
-        mutate(category = category_name,
-               subcategory = category_number)
+split_by_year_fn <- function(data){
     data_sep = data %>%
         group_by(year)
     sep_list = dplyr::group_split(data_sep)
@@ -37,44 +47,51 @@ split_by_year_fn <- function(data,
     return(sep_list)
 }
 
-
 # Individual data 1 -------------------------------------------------------
-df_ind1 <- database2[["Individual_part1"]]
+df_ind1 <- database[["Individual_part1"]]
 # have the similar variable to SA1 data
 df_ind1$variable_name %>%  unique()
 df_ind1 <- df_ind1 %>%
     mutate(variable_name = gsub("\032", "a", variable_name, fixed = TRUE))
-a_list <- split_by_year_fn(df_ind1, category_number = "1")
+df_ind1 <- fn_correct_variables(df_ind1, "1")
+
+a_list <- split_by_year_fn(df_ind1)
 
 # a_list[["2018"]] %>%  View()
 
 
 #  individual part 2 ------------------------------------------------------
-df_ind2 <- database2[["Individual_part2"]]
+df_ind2 <- database[["Individual_part2"]]
 df_ind2$variable_name %>%  unique()
-b_list <- split_by_year_fn(df_ind2, category_number = "2")
+df_ind2 <- fn_correct_variables(df_ind2, "2")
+
+b_list <- split_by_year_fn(df_ind2)
 # b_list[["2018"]] %>%  View()
 
-
 #  individual part 3a ----------------------------------------------------
-df_ind3a <- database2[["Individual_part3a"]]
+df_ind3a <- database[["Individual_part3a"]]
 df_ind3a$variable_name %>%  unique()
-c_list <- split_by_year_fn(df_ind3a, category_number = "3a")
+df_ind3a <- fn_correct_variables(df_ind3a, "3a")
+
+c_list <- split_by_year_fn(df_ind3a)
 
 
 #  Individual part3b -----------------------------------------------------
-df_ind3b <- database2[["Individual_part3b"]]
+df_ind3b <- database[["Individual_part3b"]]
 df_ind3b$variable_name %>%  unique()
-d_list <- split_by_year_fn(df_ind3b, category_number = "3b")
+df_ind3b <- fn_correct_variables(df_ind3b, "3b")
+
+d_list <- split_by_year_fn(df_ind3b)
 # d_list[["2018"]] %>%  View()
 
 
 # Individual part 4 -------------------------------------------------------
-df_ind4 <- database2[["Individual_part4"]]
+df_ind4 <- database[["Individual_part4"]]
 df_ind4$variable_name %>%  unique()
-e_list <- split_by_year_fn(df_ind4, category_number = "4")
-# e_list[["2018"]] %>%  View()
+df_ind4 <- fn_correct_variables(df_ind4, "4")
 
+e_list <- split_by_year_fn(df_ind4)
+# e_list[["2018"]] %>%  View()
 
 # organise the data by year -----------------------------------------------
 # a_list %>%  names()
